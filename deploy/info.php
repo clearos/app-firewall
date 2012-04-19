ca4e1ebc0d7c50f0a083e0770f1d50674633b7ef
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-19 21:14:08 +0000

    - Bumped version for release

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.1.0';
+$app['version'] = '1.1.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-firewall.spec
+++ packaging/app-firewall.spec
@@ -1,7 +1,7 @@
 
 Name: app-firewall
 Epoch: 1
-Version: 1.1.0
+Version: 1.1.1
 Release: 1%{dist}
 Summary: Firewall - Core
 License: LGPLv3
