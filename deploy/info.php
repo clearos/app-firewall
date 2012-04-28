406cd0f5635e6d7ad6b49b75ff0a5682b9b4eb26
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-28 01:50:45 +0000

    - Prepped for bug fix update

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.1.1';
+$app['version'] = '1.1.2';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
