4506041beb1b9fc004f426541d468f63c52fbb33
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-30 21:35:07 +0000

    - Prepped version for pre-beta 1 build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.4.5';
+$app['version'] = '1.4.7';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
