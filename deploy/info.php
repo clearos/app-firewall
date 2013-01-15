13156d65750b87f83d6f9bf4996073272f8f46af
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2013-01-15 19:47:51 +0000

    - Bumped version.

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.4.14';
+$app['version'] = '1.4.15';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-firewall.spec
+++ packaging/app-firewall.spec
@@ -1,7 +1,7 @@
 
 Name: app-firewall
 Epoch: 1
-Version: 1.4.14
+Version: 1.4.15
 Release: 1%{dist}
 Summary: Firewall - Core
 License: LGPLv3
