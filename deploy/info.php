4b54f42fb137fd6cbcc68ece9e3d36d625867fe9
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-03-27 15:28:19 +0000

    - Prepped for 6.4.0 Final

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.4.20';
+$app['version'] = '1.4.30';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
