12ee6545564ad94f0e7951cc86049d82694387c3
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2013-07-03 23:35:57 +0000

    - Added missing QOS_UP/DOWNSTREAM_BWRES/LIMIT variables.

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.4.36';
+$app['version'] = '1.4.37';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-firewall.spec
+++ packaging/app-firewall.spec
@@ -1,7 +1,7 @@
 
 Name: app-firewall
 Epoch: 1
-Version: 1.4.36
+Version: 1.4.37
 Release: 1%{dist}
 Summary: Firewall - Core
 License: LGPLv3
