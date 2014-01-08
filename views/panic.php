3cb0b71d06a79d2352dbe32b9ae2e0b59c3d7675
Ben Chambers <bchambers@clearfoundation.com>
2014-01-08 18:10:59 +0000

    - Missed adding view

---
+++ views/panic.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Firewall panic mode notification view.
+ *
+ * @category   apps
+ * @package    firewall
+ * @subpackage views
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2013 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/firewall/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Load dependencies
+///////////////////////////////////////////////////////////////////////////////
+
+$this->lang->load('firewall');
+$this->lang->load('base');
+
+echo infobox_warning(lang('base_warning'), lang('firewall_panic'));
