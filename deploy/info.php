3f3da84a4eede76f91010f6a2f957507a384cb6c
pcbaldwin <pbaldwin@clearfoundation.com>
2014-05-08 20:37:37 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.5.20';
+$app['version'] = '1.6.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-firewall.spec
+++ packaging/app-firewall.spec
@@ -1,7 +1,7 @@
 
 Name: app-firewall
 Epoch: 1
-Version: 1.5.20
+Version: 1.6.0
 Release: 1%{dist}
 Summary: Firewall
 License: GPLv3
