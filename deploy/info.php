901cc8410497198ca1ff8cedd9bfb0813acb96d4
pcbaldwin <pbaldwin@clearfoundation.com>
2014-06-20 17:25:59 -0400

    Redirected chkconfig/service to devnull (cosmetic)

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '1.6.0';
+$app['version'] = '1.6.5';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-firewall.spec
+++ packaging/app-firewall.spec
@@ -1,7 +1,7 @@
 
 Name: app-firewall
 Epoch: 1
-Version: 1.6.0
+Version: 1.6.5
 Release: 1%{dist}
 Summary: Firewall
 License: GPLv3
