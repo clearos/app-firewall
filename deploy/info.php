14f3c6802b0f52b5dbf465973d00f982dc2cce70
pcbaldwin <pbaldwin@clearfoundation.com>
2015-03-27 14:04:32 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '2.0.20';
+$app['version'] = '2.0.21';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
