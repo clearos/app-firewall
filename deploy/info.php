a6b63eb0a5d34a0425b8ed74d58dceafd51b9f3a
pcbaldwin <pbaldwin@clearfoundation.com>
2015-11-22 21:38:29 -0500

    Bumped version for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '2.1.25';
+$app['version'] = '2.1.26';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-firewall.spec
+++ packaging/app-firewall.spec
@@ -1,7 +1,7 @@
 
 Name: app-firewall
 Epoch: 1
-Version: 2.1.25
+Version: 2.1.26
 Release: 1%{dist}
 Summary: Firewall
 License: GPLv3
