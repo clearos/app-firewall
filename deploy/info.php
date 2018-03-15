8e0fba3d1512c2c3c35c68130bfdcf7410ef9721
pcbaldwin <peter@egloo.ca>
2018-03-15 11:02:49 -0400

    Buildsys rebuild

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'firewall';
-$app['version'] = '2.4.24';
+$app['version'] = '2.4.25';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-firewall.spec
+++ packaging/app-firewall.spec
@@ -1,7 +1,7 @@
 
 Name: app-firewall
 Epoch: 1
-Version: 2.4.24
+Version: 2.4.25
 Release: 1%{dist}
 Summary: Firewall
 License: GPLv3
